18ffb799e2b5207cea5fa4569d9d6c0915499651
fgutierrezme <felipemalo@MacBook-Air.local>
2025-06-22 11:41:02 -0400

    Probando git en mi proyecto

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+  // var myName = 'Felipe';
+  // late final  myName;
+  // String myName = 'Felipe';
+  const myName = 'Felipe';
+  
+  
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  
+}
